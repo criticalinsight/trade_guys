763c441134b51875f3b068a698c91c3bd11b5284
aleesteele <aleesteele@gmail.com>
2019-02-25 14:00:16 -0500

    intitial waypoints setup

--- assets/_js/custom-viz/boeing/waypoints.js
+++ assets/_js/custom-viz/boeing/waypoints.js
@@ -4,6 +4,29 @@ import 'waypoints/src/shortcuts/sticky'
 
 const Waypoints = () => {
   console.log('waypoint file')
+
+  const paragraphs = Array.from(document.querySelectorAll('.scroll-text'))
+  paragraphs.forEach(paragraph => {
+    const parts = paragraph.querySelectorAll('.part')
+    new Waypoint({
+      element: paragraph,
+      handler: function(direction) {
+        Array.from(parts).forEach(part => {
+          let partName = part.getAttribute('data-part')
+          if (direction == 'down') {
+            setTimeout(function run() {
+              console.log('down: ' + partName)
+
+              setTimeout(run, 100)
+            }, 1000)
+          } else if (direction == 'up') {
+            console.log('up: ' + partName)
+          }
+        })
+      },
+      offset: '70%'
+    })
+  })
 }
 
 export default Waypoints
