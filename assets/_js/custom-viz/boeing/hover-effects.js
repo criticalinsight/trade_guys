c49331530c7580689822e29988f1d0f5fdc7be95
dlousa92 <dlousa92@gmail.com>
2019-06-07 16:49:47 -0400

    add effect when hovering out of label

--- assets/_js/custom-viz/boeing/hover-effects.js
+++ assets/_js/custom-viz/boeing/hover-effects.js
@@ -4,14 +4,33 @@ const Hover = () => {
   paragraphs.forEach(paragraph => {
     let activeParts = Array.from(paragraph.querySelectorAll('.part'))
     activeParts.forEach(function(part) {
+      let partName
+      let partSVG
+      let partLabel
+
       part.addEventListener('mouseover', () => {
         const notHovered = activeParts.filter(partName => partName !== part)
 
         notHovered.forEach(part => {
-          let partName = part.getAttribute('data-part')
-          let partSVG = document.getElementById(`${partName}-part`)
+          partName = part.getAttribute('data-part')
+          partLabel = document.getElementById(`${partName}-label`)
+          partSVG = document.getElementById(`${partName}-part`)
 
+          partLabel.classList.remove('boeing-js__opacity-focused')
           partSVG.classList.remove('boeing-js__opacity-focused')
+        })
+      })
+
+      part.addEventListener('mouseout', () => {
+        const notHovered = activeParts.filter(partName => partName !== part)
+
+        notHovered.forEach(part => {
+          partName = part.getAttribute('data-part')
+          partLabel = document.getElementById(`${partName}-label`)
+          partSVG = document.getElementById(`${partName}-part`)
+
+          partLabel.classList.add('boeing-js__opacity-focused')
+          partSVG.classList.add('boeing-js__opacity-focused')
         })
       })
     })
